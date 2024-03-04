f7bf40407ea28c14441d7df209bb93397a1bd8f5
YoungQWER <wkdrnr321@naver.com>
2024-03-04 14:53:38 +0900

    second

--- web-study-01/src/main/java/unit01/AdditonServlet01.java
+++ web-study-01/src/main/java/unit01/AdditonServlet01.java
@@ -9,21 +9,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class AdditonServlet01
- */
 @WebServlet("/AdditonServlet01")
 public class AdditonServlet01 extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public AdditonServlet01() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
- 
+
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		int num1 = 10;
 		int num2 = 20;
@@ -37,9 +26,6 @@ public class AdditonServlet01 extends HttpServlet {
 		out.println("</body></html>");
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 	}
---
+++ web-study-03/src/main/webapp/footer.jsp
@@ -0,0 +1,8 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<hr>
+<div id="copyright">
+	All contents Copyright 2024
+	Contact mail : test@gmail.com
+</div>
